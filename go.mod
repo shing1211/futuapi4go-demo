d81e8b5ee8e05efc49211c00375ca892d1cbdf7f
Terence Chan <shing1211@gmail.com>
2026-04-24 00:13:47 +0800

    demo: use SubscribeKLines for multi-period K-line routing

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/shing1211/futuapi4go-demo
 
 go 1.26.1
 
-require github.com/shing1211/futuapi4go v0.9.5
+require github.com/shing1211/futuapi4go v0.9.6
 
 require google.golang.org/protobuf v1.36.11 // indirect
--- main.go
+++ main.go
@@ -26,9 +26,18 @@ func main() {
 	orderBookCh := make(chan *push.UpdateOrderBook, 100)
 	rtCh := make(chan *push.UpdateRT, 100)
 	brokerCh := make(chan *push.UpdateBroker, 100)
-	klCh := make(chan *push.UpdateKL, 100)
 
 	chanpkg.SubscribeQuote(cli, constant.Market_US, "NVDA", quoteCh)
+	chanpkg.SubscribeTicker(cli, constant.Market_US, "NVDA", tickerCh)
+	chanpkg.SubscribeOrderBook(cli, constant.Market_US, "NVDA", orderBookCh)
+	chanpkg.SubscribeRT(cli, constant.Market_US, "NVDA", rtCh)
+	chanpkg.SubscribeBroker(cli, constant.Market_US, "NVDA", brokerCh)
+	klCh1, klCh5, stopKLs := func() (chan *push.UpdateKL, chan *push.UpdateKL, func()) {
+		channels, stop := chanpkg.SubscribeKLines(cli, constant.Market_US, "NVDA",
+			constant.KLType_K_1Min, constant.KLType_K_5Min)
+		return channels[constant.KLType_K_1Min], channels[constant.KLType_K_5Min], stop
+	}()
+	defer stopKLs()
 
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
@@ -59,9 +68,14 @@ func main() {
 				fmt.Printf("BROKER: name=%s pos=%d\n",
 					b.BidBrokerList[0].GetName(), b.BidBrokerList[0].GetPos())
 			}
-		case kl := <-klCh:
-			for _, bar := range kl.KLList {
-				fmt.Printf("KL: %s O=%.2f H=%.2f L=%.2f C=%.2f V=%d\n",
+		case kl1 := <-klCh1:
+			for _, bar := range kl1.KLList {
+				fmt.Printf("KL [1m/SubscribeKLines]: %s O=%.2f H=%.2f L=%.2f C=%.2f V=%d\n",
+					*bar.Time, *bar.OpenPrice, *bar.HighPrice, *bar.LowPrice, *bar.ClosePrice, *bar.Volume)
+			}
+		case kl5 := <-klCh5:
+			for _, bar := range kl5.KLList {
+				fmt.Printf("KL [5m/SubscribeKLines]: %s O=%.2f H=%.2f L=%.2f C=%.2f V=%d\n",
 					*bar.Time, *bar.OpenPrice, *bar.HighPrice, *bar.LowPrice, *bar.ClosePrice, *bar.Volume)
 			}
 		case <-sig:
