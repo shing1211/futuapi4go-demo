7eb0c443b14d858b1eabfd65ff08da176cd55d92
Terence Chan <shing1211@gmail.com>
2026-04-23 21:29:39 +0800

    fix: restore properly working examples
    - Keep only examples that build correctly
    - Use public client package API
    - Remove old examples that need significant rewrites

--- examples/getting_started/main.go
+++ examples/getting_started/main.go
@@ -1,4 +1,4 @@
-// Copyright 2026 shing1211
+﻿// Copyright 2026 shing1211
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
